ce5817cdbc659e7bfb4144c046485b0cf717f99d
Juliana Alvares <alvares.juliana@hotmail.com>
2023-06-05 19:50:01 -0300

    add Table component style

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import MyContext from '../context/MyContext';
+import styles from '../styles/Table.module.css';
 
 function Table() {
   const { planetsData, filteredPlanets } = useContext(MyContext);
@@ -7,28 +8,29 @@ function Table() {
   if (planetsData.length > 0) {
     const planetsInfo = Object.keys(planetsData[0]);
     return (
-      <table>
-        <caption>Planetas do Universo StarWars</caption>
-        <thead>
-          <tr>
-            { planetsInfo.map((info) => <th key={ info }>{ info }</th>) }
-          </tr>
-        </thead>
-        <tbody>
-          { filteredPlanets.map((planet) => (
-            <tr key={ planet.name }>
-              { planetsInfo.map((info, index) => (
-                <td
-                  key={ index }
-                  data-testid={ (info === 'name') ? 'planet-name' : '' }
-                >
-                  { planet[info] }
-                </td>
-              )) }
+      <div className={ styles.container }>
+        <table>
+          <thead>
+            <tr>
+              { planetsInfo.map((info) => <th key={ info }>{ info }</th>) }
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            { filteredPlanets.map((planet) => (
+              <tr key={ planet.name }>
+                { planetsInfo.map((info, index) => (
+                  <td
+                    key={ index }
+                    data-testid={ (info === 'name') ? 'planet-name' : '' }
+                  >
+                    { planet[info] }
+                  </td>
+                )) }
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
   return <span>Carregando...</span>;
