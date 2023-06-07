279911af1c7f5d8641ccb387b417c0075c2b7321
Juliana Alvares <alvares.juliana@hotmail.com>
2023-06-07 18:04:15 -0300

    create NumericFilterList component styles

--- src/components/NumericFilterList.js
+++ src/components/NumericFilterList.js
@@ -1,14 +1,25 @@
 import React, { useContext } from 'react';
 import { MdDeleteForever } from 'react-icons/md';
 import MyContext from '../context/MyContext';
+import styles from '../styles/NumericFilterList.module.css';
 
 function NumericFilterList() {
   const { filterByNumericValues,
     removeNumericFilter,
     clearNumericFilters } = useContext(MyContext);
   return (
-    <div>
-      { (filterByNumericValues.length > 0)
+    <div className={ styles.container }>
+      <button
+        type="button"
+        data-testid="button-remove-filters"
+        disabled={ filterByNumericValues.length <= 0 }
+        onClick={ clearNumericFilters }
+        className={ styles.remove }
+      >
+        REMOVE FILTERS
+      </button>
+      <div className={ styles.list }>
+        { (filterByNumericValues.length > 0)
         && filterByNumericValues.map(({ column, comparison, value }, index) => (
           <div data-testid="filter" key={ index }>
             <span>{ `${column} ${comparison} ${value}` }</span>
@@ -17,14 +28,7 @@ function NumericFilterList() {
             </button>
           </div>
         )) }
-      <button
-        type="button"
-        data-testid="button-remove-filters"
-        disabled={ filterByNumericValues.length <= 0 }
-        onClick={ clearNumericFilters }
-      >
-        Remover Filtros
-      </button>
+      </div>
     </div>
   );
 }
